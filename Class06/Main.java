46ff01adea98d588273c10351861b7db83acde75
igorgcf <igorTecnologia@outlook.com>
2025-08-04 01:00:45 -0300

    Main

---
+++ Class06/Main.java
@@ -0,0 +1,14 @@
+package Class06;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Form square = new Square();
+
+        System.out.println("*****----------------------******");
+        System.out.println("Área do quadrado: " + square.calculateArea(10));
+        System.out.println("*****----------------------******");
+
+    }
+}
